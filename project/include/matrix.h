c2ea04f3d6098d7391cf2d9514b5e64e5e059948
Artem <ayuchapaev@gmail.com>
2022-04-23 17:55:15 +0300

    Сделал ДЗ №5

--- project/include/matrix.h
+++ project/include/matrix.h
@@ -4,40 +4,61 @@
 #include <istream>
 
 namespace prep {
+    using std::vector;
+
 class Matrix {
  public:
-  explicit Matrix(size_t rows = 0, size_t cols = 0);
-  explicit Matrix(std::istream& is);
-  Matrix(const Matrix& rhs) = default;
-  Matrix& operator=(const Matrix& rhs) = default;
-  ~Matrix() = default;
+      explicit Matrix(size_t row = 0, size_t col = 0);
+      explicit Matrix(std::istream& is);
 
-  size_t getRows() const;
-  size_t getCols() const;
+      Matrix(const Matrix& rhs) = default;
+      Matrix& operator=(const Matrix& rhs) = default;
+      ~Matrix() = default;
 
-  double operator()(size_t i, size_t j) const;
-  double& operator()(size_t i, size_t j);
+      size_t getRows() const;
+      size_t getCols() const;
 
-  bool operator==(const Matrix& rhs) const;
-  bool operator!=(const Matrix& rhs) const;
+      double operator()(size_t i, size_t j) const;
+      double& operator()(size_t i, size_t j);
 
-  Matrix operator+(const Matrix& rhs) const;
-  Matrix operator-(const Matrix& rhs) const;
-  Matrix operator*(const Matrix& rhs) const;
+      bool operator==(const Matrix& rhs) const;
+      bool operator!=(const Matrix& rhs) const;
 
-  Matrix operator*(double val) const;
+      Matrix operator+(const Matrix& rhs) const;
+      Matrix operator-(const Matrix& rhs) const;
+      Matrix operator*(const Matrix& rhs) const;
 
-  friend
-  Matrix operator*(double val, const Matrix& matrix);
-  friend
-  std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+      Matrix operator*(double val) const;
 
-  Matrix transp() const;
-  double det() const;
-  Matrix adj() const;
-  Matrix inv() const;
+      friend Matrix operator*(double val, const Matrix& matrix);
+      friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+
+      Matrix transp() const;
+      double det() const;
+      Matrix adj() const;
+      Matrix inv() const;
+
+ private:
+      vector <vector<double>> matrix;
+      size_t rows;
+      size_t cols;
+
+      void createMatrix();
+
+      friend bool almostEqual(const double x, const double y);
+      friend double add(const double x, const double y);
+      friend double subtract(const double x, const double y);
+
+      void calculateMatrix(const Matrix &rhs, Matrix *NewMatrix,
+                                  double (*operationFunc)(double, double)) const;
+      Matrix performAddMatrix(const Matrix &rhs, const char operation) const;
+
+      double countDet() const;
+      void matrixPartCpy(Matrix* ptrNewMatrix, size_t row, size_t col) const;
 };
 
-Matrix operator*(double val, const Matrix& matrix);
-std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+    Matrix operator*(double val, const Matrix& matrix);
+    std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+
+    bool almostEqual(double x, double y);
 }  // namespace prep
--- project/src/matrix.cpp
+++ project/src/matrix.cpp
@@ -1,5 +1,194 @@
+#include <iomanip>
+#include <iostream>
+
 #include "matrix.h"
 #include "exceptions.h"
 
 namespace prep {
-}
+    using std::endl;
+
+    Matrix::Matrix(size_t row, size_t col) : rows(row), cols(col) {
+        createMatrix();
+    }
+
+    Matrix::Matrix(std::istream &is) {
+        is >> rows >> cols;
+        if (!is) {
+            throw InvalidMatrixStream();
+        }
+
+        createMatrix();
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                is >> matrix[i][j];
+
+                if (!is) {
+                    throw InvalidMatrixStream();
+                }
+            }
+        }
+    }
+
+    size_t Matrix::getRows() const { return rows; }
+
+    size_t Matrix::getCols() const { return cols; }
+
+    double Matrix::operator()(size_t i, size_t j) const {
+        if (i >= rows || j >= cols) {
+            throw OutOfRange(i, j, *this);
+        }
+        return matrix[i][j];
+    }
+
+    double& Matrix::operator()(size_t i, size_t j) {
+        if (i >= rows || j >= cols) {
+            throw OutOfRange(i, j, *this);
+        }
+        return matrix[i][j];
+    }
+
+    Matrix Matrix::operator+(const Matrix &rhs) const {
+        return performAddMatrix(rhs, '+');
+    }
+
+    Matrix Matrix::operator-(const Matrix &rhs) const {
+        return performAddMatrix(rhs, '-');
+    }
+
+    Matrix Matrix::operator*(const Matrix &rhs) const {
+        if (cols != rhs.rows) {
+            throw DimensionMismatch(*this, rhs);
+        }
+
+        Matrix newMatrix(rows, rhs.cols);
+
+        for (size_t i = 0; i < newMatrix.rows; i++) {
+            for (size_t j = 0; j < newMatrix.cols; j++) {
+                double value = 0;
+
+                for (size_t k = 0; k < cols; k++) {
+                    value += matrix[i][k] * rhs.matrix[k][j];
+                }
+
+                newMatrix.matrix[i][j] = value;
+            }
+        }
+        return newMatrix;
+    }
+
+    bool Matrix::operator==(const Matrix &rhs) const {
+        if (rows != rhs.rows || cols != rhs.cols) {
+            return false;
+        }
+
+        bool equal = true;
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                if (!almostEqual(matrix[i][j], rhs.matrix[i][j])) {
+                    equal = false;
+                    break;
+                }
+            }
+            if (!equal) {
+                break;
+            }
+        }
+        return equal;
+    }
+
+    bool Matrix::operator!=(const Matrix &rhs) const { return !(*this == rhs); }
+
+    Matrix Matrix::operator*(double val) const {
+        Matrix newMatrix(rows, cols);
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                newMatrix.matrix[i][j] = matrix[i][j] * val;
+            }
+        }
+        return newMatrix;
+    }
+
+    Matrix operator*(double val, const Matrix &matrix) { return matrix * val; }
+
+    std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
+        os << matrix.rows << ' ' << matrix.cols << endl;
+
+        for (size_t i = 0; i < matrix.rows; ++i) {
+            for (size_t j = 0; j < matrix.cols; ++j) {
+                os << std::setprecision(9) << matrix.matrix[i][j];
+                if (j != matrix.cols - 1) {
+                    os << ' ';
+                }
+            }
+            if (i != matrix.rows - 1) {
+                os << endl;
+            }
+        }
+        return os;
+    }
+
+    Matrix Matrix::transp() const {
+        Matrix newMatrix(cols, rows);
+
+        for (size_t i = 0; i < newMatrix.rows; i++) {
+            for (size_t j = 0; j < newMatrix.cols; j++) {
+                newMatrix.matrix[i][j] = matrix[j][i];
+            }
+        }
+
+        return newMatrix;
+    }
+
+    double Matrix::det() const {
+        if (rows != cols) {
+            throw DimensionMismatch(*this);
+        }
+        return countDet();
+    }
+
+    Matrix Matrix::adj() const {
+        if (rows != cols) {
+            throw DimensionMismatch(*this);
+        }
+
+        Matrix adjMatrix(rows, cols);
+
+        if (rows == 1) {
+            adjMatrix.matrix[0][0] = 1;
+            return adjMatrix;
+        }
+
+        Matrix partOfMatrix(cols - 1, rows - 1);
+
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                matrixPartCpy(&partOfMatrix, j, i);  // поменял i и j местами для транспонирования
+
+                adjMatrix.matrix[i][j] = partOfMatrix.countDet();
+                if ((i + j) % 2 == 1) {
+                    adjMatrix.matrix[i][j] *= -1;
+                }
+            }
+        }
+
+        return adjMatrix;
+    }
+
+    Matrix Matrix::inv() const {
+        if (rows != cols) {
+            throw DimensionMismatch(*this);
+        }
+
+        double determinant = det();
+        if (determinant == 0) {
+            throw SingularMatrix();
+        }
+
+        Matrix invMatrix = adj() * (1 / determinant);
+
+        return invMatrix;
+    }
+}  // namespace prep
